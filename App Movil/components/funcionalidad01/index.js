9ab5ffb28cb09fa5eab3de6a7b9a9f86991b7e26
diego corrada <diego.corrada@valmar.com.pe>
2015-10-06 19:33:44 +0000

    Changes made by diego corrada (diego.corrada@valmar.com.pe).

--- App Movil/components/funcionalidad01/index.js
+++ App Movil/components/funcionalidad01/index.js
@@ -6,4 +6,22 @@ app.funcionalidad01 = kendo.observable({
 });
 
 // START_CUSTOM_CODE_funcionalidad01
+
+var foo = new kendo.data.DataSource({ 
+   data: [
+        "ADIDAS", 
+        "HUAWUEI", 
+        "ADM",
+        "PHILIPS", 
+        "COSAPI", 
+        "ASTRAZENECA PERU S.A.",
+        "RENA WARE DEL PERU' S.A.", 
+        "NIKE", 
+        "SONY",
+        "HONDA DEL PERU' S.A.", 
+        "DAMCO S.A.",
+        "COCA COLA"
+    ] 
+});
+
 // END_CUSTOM_CODE_funcionalidad01
