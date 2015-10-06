61ec78f6cf671b19069bc5f647c35640efb9db2b
Erik Gadea <erik.gadea@valmar.com.pe>
2015-10-06 14:45:34 +0000

    Changes made by Erik Gadea (erik.gadea@valmar.com.pe).

--- App Movil/components/funcionalidad02/index.js
+++ App Movil/components/funcionalidad02/index.js
@@ -1,9 +1,9 @@
-'use strict';
+//'use strict';
 
-app.funcionalidad02 = kendo.observable({
-    onShow: function() {},
-    afterShow: function() {}
-});
+//app.funcionalidad02 = kendo.observable({
+//    onShow: function() {},
+//    afterShow: function() {}
+//});
 
 // START_CUSTOM_CODE_funcionalidad02
 // END_CUSTOM_CODE_funcionalidad02
