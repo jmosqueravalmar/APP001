a1695a1230dc76a8252463edc8fb846a8c0b3d2c
diego corrada <diego.corrada@valmar.com.pe>
2015-10-07 16:29:08 +0000

    Changes made by diego corrada (diego.corrada@valmar.com.pe).

--- App Movil/components/funcionalidad01/index.js
+++ App Movil/components/funcionalidad01/index.js
@@ -9,23 +9,35 @@ app.funcionalidad01 = kendo.observable({
 
 var foo = new kendo.data.DataSource({ 
    data: [
-        "ADIDAS", 
-        "HUAWUEI", 
-        "ADM",
-        "PHILIPS", 
-        "COSAPI", 
-        "ASTRAZENECA PERU S.A.",
-        "RENA WARE DEL PERU' S.A.", 
-        "NIKE", 
-        "SONY",
-        "HONDA DEL PERU' S.A.", 
-        "DAMCO S.A.",
-        "COCA COLA"
+        {id: 1, nombre: "ADIDAS"},
+        {id: 2, nombre: "HUAWUEI"}, 
+        {id: 3, nombre: "ADM"},
+        {id: 4, nombre: "PHILIPS"}, 
+        {id: 5, nombre: "COSAPI"}, 
+        {id: 6, nombre: "ASTRAZENECA PERU S.A."},
+        {id: 7, nombre: "RENA WARE DEL PERU' S.A."}, 
+        {id: 8, nombre: "NIKE"}, 
+        {id: 9, nombre: "SONY"},
+        {id: 10, nombre: "HONDA DEL PERU' S.A."}, 
+        {id: 11, nombre: "DAMCO S.A."},
+        {id: 12, nombre: "COCA COLA"}
     ] 
 });
 
-function listViewClick(e) {
-    console.log(e.item.Title); // The clicked item as a jQuery object
+// The clicked item as a jQuery object
+function onTap(e) {
+    console.log(e.touch.target + " was tapped");
 }
+
+$("#MyListView").kendoListView({
+    dataSource: foo,
+    template: kendo.template($("#foo-template").html()),
+    selectable: true,
+    change: function() {
+        var index = this.select().index(),
+            dataItem = this.dataSource.view()[index];
+        log("id: " + dataItem.id + ", text: " + dataItem.text);        
+    }
+});
 
 // END_CUSTOM_CODE_funcionalidad01
