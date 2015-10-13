b5e2e579ff1fe60adf46cd9f324b405448028736
diego corrada <diego.corrada@valmar.com.pe>
2015-10-13 22:38:15 +0000

    Changes made by diego corrada (diego.corrada@valmar.com.pe).

--- App Movil/components/funcionalidad01/index.js
+++ App Movil/components/funcionalidad01/index.js
@@ -1,51 +1,34 @@
 'use strict';
 
 app.funcionalidad01 = kendo.observable({
-    onShow: function() {
-       $("#ListaEmpresas").kendoMobileListView({
-            dataSource: f01,
-            template : "#:nombre#",
-            dataBound: onDataBound,    
-            click: muestraDetalle    
-       });
-    },
-    afterShow: function() {}    
+    onShow: function() {},
+    afterShow: function() {},    
 });
 
 // START_CUSTOM_CODE_funcionalidad01
 
 var f01 = new kendo.data.DataSource({ 
-   data: [
-        {id: 1, nombre: "ADIDAS"},
-        {id: 2, nombre: "HUAWUEI"}, 
-        {id: 3, nombre: "ADM"},
-        {id: 4, nombre: "PHILIPS"}, 
-        {id: 5, nombre: "COSAPI"}, 
-        {id: 6, nombre: "ASTRAZENECA PERU S.A."},
-        {id: 7, nombre: "RENA WARE DEL PERU' S.A."}, 
-        {id: 8, nombre: "NIKE"}, 
-        {id: 9, nombre: "SONY"},
-        {id: 10, nombre: "HONDA DEL PERU' S.A."}, 
-        {id: 11, nombre: "DAMCO S.A."},
-        {id: 12, nombre: "COCA COLA"},
-        {id: 13, nombre: "LAS CANASTA"},
-        {id: 14, nombre: "EL COMERCIO"}
-    ] 
+data: [
+        {id: 1, nombre: "ADIDAS", rubro: "Sportware and Footware"},
+        {id: 2, nombre: "HUAWUEI", rubro: "Smartphone and Tablet"}, 
+        {id: 3, nombre: "ADM", rubro: "ACME ACME"},
+        {id: 4, nombre: "PHILIPS", rubro: "TV-LCD and Equipos de sonido"}, 
+        {id: 5, nombre: "COSAPI", rubro: "Consutoria empresarial"}, 
+        {id: 6, nombre: "ASTRAZENECA PERU S.A.", rubro: "Medicamentos"},
+        {id: 7, nombre: "RENA WARE DEL PERU' S.A.", rubro: "ACME ACME"}, 
+        {id: 8, nombre: "NIKE", rubro: "Sportware and Footware"}, 
+        {id: 9, nombre: "SONY", rubro: "TV-LCD and Equipos de sonido"},
+        {id: 10, nombre: "HONDA DEL PERU' S.A.", rubro: "Auto and Motos"}, 
+        {id: 11, nombre: "DAMCO S.A.", rubro: "ACME ACME"},
+        {id: 12, nombre: "COCA COLA", rubro: "Drinks and Merchandaising"}
+    ]
 });
 
-// The clicked item as a jQuery object
-
-function onDataBound() {
-    console.log("ListView data bound");
-}
+function muestraDetalle(id){
+    console.log("Muestra Detalle de:" + id);
+    var dataItem = f01.get(id);
+    console.log("Nombre: " + dataItem.nombre + " Rubro: " + dataItem.rubro);
     
-function muestraDetalle() {
-    console.log("ListView click");    
-    $("#ListaEmpresas").hide();
-    $("#DetalleEmpresa").show();
-    $("#ListaClientes").kendoMobileView({
-        dataTitle : "Detalle CLI"
-    });
 }
 
 // END_CUSTOM_CODE_funcionalidad01
