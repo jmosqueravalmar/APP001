97444e9ed8e93ef2d72832201132b03a390da2ac
Josue Emanuel Mosquera <josue.mosquera@valmar.com.pe>
2015-10-13 21:14:50 +0000

    Changes made by Josue Emanuel Mosquera (josue.mosquera@valmar.com.pe).

--- App Movil/components/funcionalidad03/index.js
+++ App Movil/components/funcionalidad03/index.js
@@ -2,10 +2,37 @@
 
 app.funcionalidad03 = kendo.observable({
                                            onShow: function() {
+                                               var dataSource = new kendo.data.DataSource({
+                                                                                              transport: {
+                                                       read: {
+                                                                                                          url: "http://demos.telerik.com/kendo-ui/service/Products",
+                                                                                                          dataType: "jsonp"
+                                                                                                      }
+                                                   } 
+                                                                                          });
+
+                                               /* $("#pager").kendoPager({
+                                               dataSource: dataSource
+                                               });*/
+
+                                               $("#tareas").kendoListView({
+                                                                              dataSource: dataSource, 
+                                                                              template: kendo.template($("#tema001").html())
+                                                                          });
                                            },
                                            afterShow: function() {
                                            }
+        
                                        });
 
 // START_CUSTOM_CODE_funcionalidad03
+ 
+function llamada(e) {
+    //alert(e);
+    
+    //$("#nombre").append(" <b>Id del registro - "+ e +"</b>.");    
+    document.getElementById("nombre").innerHTML = " <b>Id del registro - "+ e +"</b>.";
+    
+    window.location.href = "#detalleTarea";
+} 
 // END_CUSTOM_CODE_funcionalidad03
