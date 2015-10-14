211a290241c44d734fed45fe309f9fa2c78d1fc6
Josue Emanuel Mosquera <josue.mosquera@valmar.com.pe>
2015-10-14 14:37:27 +0000

    Changes made by Josue Emanuel Mosquera (josue.mosquera@valmar.com.pe).

--- App Movil/components/funcionalidad03/index.js
+++ App Movil/components/funcionalidad03/index.js
@@ -1,89 +1,89 @@
 'use strict';
-
 app.funcionalidad03 = kendo.observable({
-                                           onShow: function() {
-                                               var dataSource = new kendo.data.DataSource({
-                                                                                              type: "odata",
-                                                                                              transport: {
-                                                       read: {
-                                                                                                          url: "http://demos.telerik.com/kendo-ui/service/Northwind.svc/Employees"
+    onShow: function () {
+        var dataSource = new kendo.data.DataSource({
+            type: "odata",
+            transport: {
+                read: {
+                    url: "http://demos.telerik.com/kendo-ui/service/Northwind.svc/Employees"
+                },
+                dataBound: function () {
+                    this.expandRow(this.tbody.find("tr.k-master-row").first());
+                },
+                columns: [{
+                        field: "FirstName",
+                        title: "First Name"
+                        }, {
+                        field: "LastName",
+                        title: "Last Name"
+                        }, {
+                        field: "Country"
+                        }, {
+                        field: "City"
+                        }, {
+                        field: "Title"
+                        }]
+            }
+        });
 
-                                                                                                      },
-                                                       dataBound: function() {
-                                                           this.expandRow(this.tbody.find("tr.k-master-row").first());
-                                                       },
-                                                       columns: [{
-                                                                       field: "FirstName",
-                                                                       title: "First Name"
-                                                                   },{
-                                                                       field: "LastName",
-                                                                       title: "Last Name"
-                                                                   },{
-                                                                       field: "Country"
-                                                                   },{
-                                                                       field: "City"
-                                                                   },{
-                                                                       field: "Title"
-                                                                   }
-                                                                                                      ]
-                                                   } 
-                                                                                          });
+        /* $("#pager").kendoPager({
+        dataSource: dataSource
+        });*/
 
-                                               /* $("#pager").kendoPager({
-                                               dataSource: dataSource
-                                               });*/
-
-                                               $("#tareas").kendoListView({
-                                                                              dataSource: dataSource, 
-                                                                              template: kendo.template($("#tema001").html())
-                                                                          });
-                                           },
-                                           afterShow: function() {
-                                           }
-
-                                       });
+        $("#tareas").kendoListView({
+            dataSource: dataSource,
+            template: kendo.template($("#tema001").html())
+        });
+    },
+    afterShow: function () {}
+});
 
 // START_CUSTOM_CODE_funcionalidad03
-
-function detailInit(e) {
-    alert(e);
+function detailInit(idEmpleado) {
+    //alert(idEmpleado);
     var dataSourceDetail = new kendo.data.DataSource({
-                                                         type: "odata",
-                                                         transport: {
+        type: "odata",
+        transport: {
             read: {
-                                                                     read: "http://demos.telerik.com/kendo-ui/service/Northwind.svc/Orders"
+                read: "http://demos.telerik.com/kendo-ui/service/Northwind.svc/Orders"
 
-                                                                 },
-            filter: { field: "EmployeeID", operator: "eq", value: e },    
+            },
+            filter: {
+                field: "EmployeeID",
+                operator: "eq",
+                value: idEmpleado
+            },
             columns: [{
-                            field: "OrderID",
-                            title: "OrderID."
-                        },{
-                            field: "ShipCountry",
-                            title: "ShipCountry."
-                        },{
-                            field: "ShipAddress"
-                        },{
-                            field: "ShipName"
-                        }
-                                                                 ]
-        } 
-                                                     });
-                 
+                    field: "OrderID",
+                    title: "OrderID."
+                        }, {
+                    field: "ShipCountry",
+                    title: "ShipCountry."
+                        }, {
+                    field: "ShipAddress"
+                        }, {
+                    field: "ShipName"
+                        }]
+        }
+    });
+    
+    $("#nombre2").text(dataSourceDetail);
+    
     $("#detalle2").kendoListView({
-                                     dataSource: dataSourceDetail, 
-                                     template: kendo.template($("#tema002").html())
-                                 }); 
-    }; 
+        dataSource: dataSourceDetail,
+        template: kendo.template($("#tema002").html())
+    });
+};
 
-    window.llamada = function(e) { 
-        //alert(e);
-        var seleccionado = e.view.params.info;
-        var seleccionado2 = e.view.params.info2;
-        $("#nombre").text(seleccionado); 
-        $("#nombre2").text(seleccionado2); 
-        
-        detailInit(seleccionado);
-        
-    } 
+window.llamada = function (e) {
+    //alert(e);
+    var seleccionado = e.view.params.info;
+    var seleccionado2 = e.view.params.info2;
+    $("#nombre").text(seleccionado);
+    $("#nombre2").text(seleccionado2);
+
+    detailInit(seleccionado);
+
+}
+
 // END_CUSTOM_CODE_funcionalidad03
