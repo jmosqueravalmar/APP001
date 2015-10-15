92e815977a8dd97fdde7738f87b6b1633811b0d9
Erik Gadea <erik.gadea@valmar.com.pe>
2015-10-15 20:43:21 +0000

    Changes made by Erik Gadea (erik.gadea@valmar.com.pe).

--- App Movil/components/funcionalidad02/index.js
+++ App Movil/components/funcionalidad02/index.js
@@ -1,79 +1,39 @@
 document.addEventListener("deviceready", onDeviceReady, false);
  
-function id(element) {
-    return document.getElementById(element);
-}
 
 function onDeviceReady() {
+    navigator.notification.alert(navigator.device.capture);
     navigator.splashscreen.hide();
     captureApp = new captureApp();
     captureApp.run();
+    //navigator.notification.alert("entro");
 }
 
 function captureApp() {
 }
 
-captureApp.prototype = {
-    pictureSource:null,
-    
-    destinationType:null,
-    
-    run:function() {
-        var that = this;
-        id("captureVideo").addEventListener("click", function() {
-            that._captureVideo.apply(that, arguments);
-        });
-        id("captureAudio").addEventListener("click", function() {
-            that._capureAudio.apply(that, arguments);
-        });
-        id("captureImage").addEventListener("click", function() {
-            that._captureImage.apply(that, arguments);
-        });
-    },
-    
-    _captureVideo:function() {
-        var that = this;
-        navigator.device.capture.captureVideo(function() {
-            that._captureSuccess.apply(that, arguments);
-        }, function() { 
-            captureApp._captureError.apply(that, arguments);
-        }, {limit:1});
-    },
-    
-    _capureAudio:function() {
-        var that = this;
-        navigator.device.capture.captureAudio(function() {
-            that._captureSuccess.apply(that, arguments);
-        }, function() { 
-            captureApp._captureError.apply(that, arguments);
-        }, {limit:1});
-    },
-    
-    _captureImage:function() {
-        var that = this;
-        navigator.device.capture.captureImage(function() {
-            that._captureSuccess.apply(that, arguments);
-        }, function() { 
-            captureApp._captureError.apply(that, arguments);
-        }, {limit:1});
-    },
-    
-    _captureSuccess:function(capturedFiles) {
-        var i,
-        media = document.getElementById("media");
-        media.innerHTML = "";
-        for (i=0;i < capturedFiles.length;i+=1) {
-            media.innerHTML+='<p>Capture taken! Its path is: ' + capturedFiles[i].fullPath + '</p>'
-        }
-    },
-    
-    _captureError:function(error) {
-        if (window.navigator.simulator === true) {
-            alert(error);
-        }
-        else {
-            var media = document.getElementById("media");
-            media.innerHTML = "An error occured! Code:" + error.code;
-        }
-    },
-}
+//captureApp.prototype = {
+  //  navigator.notification.alert("entro");
+//}
+
+
+   window.audio = kendo.observable({
+     seleccionar: function() {
+         //navigator.notification.alert("entro");
+         var that = this;
+         navigator.device.capture.captureAudio(function() {
+         //that._captureSuccess.apply(that, arguments);
+                var i,
+                media = document.getElementById("media");
+                media.innerHTML = "";
+                for (i=0;i < capturedFiles.length;i+=1) {
+                    media.innerHTML+='<p>Capture taken! Its path is: ' + capturedFiles[i].fullPath + '</p>'
+                }
+           
+         }, function() {
+             navigator.notification.alert("Problemas con el APP");
+         }, {limit:1});
+
+      }
+    });
+
