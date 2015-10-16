f652129acc8c6011f55cd0f2ad12b4f94ef6e5ab
Josue Emanuel Mosquera <josue.mosquera@valmar.com.pe>
2015-10-16 22:18:36 +0000

    Changes made by Josue Emanuel Mosquera (josue.mosquera@valmar.com.pe).

--- App Movil/components/funcionalidad03/index.js
+++ App Movil/components/funcionalidad03/index.js
@@ -13,24 +13,50 @@ app.funcionalidad03 = kendo.observable({
     //Carga JavaScript 1st
 	function cargaEmpleados (){         
         var datosEmpleados = new kendo.data.DataSource({
-                type: "odata",
                 transport: {
-                    read: "http://demos.telerik.com/kendo-ui/service/Northwind.svc/Employees"
-                    ,
-                    dataBound: function () {
-                        this.expandRow(this.tbody.find("tr.k-master-row").first());
+                    read: "http://54.207.102.124:8980/Customer/rest/Customer/Read",
+                    dataType: "jsonp"
+                    },
+            	schema: {
+                    data: function (data) {
+                    	return data.response.dsCustomer.dsCustomer.ttCustomer;
                     }
                 }
             });
 
-            $("#tareas").kendoListView({
-                dataSource: datosEmpleados,
-                template: kendo.template($("#tema001").html())
-            });
+        $("#tareas").kendoListView({
+            dataSource: datosEmpleados,
+            template: kendo.template($("#tema001").html())
+        });
     }
 
 	function visibles(valor){
-        $('#'+valor).toggle();
+        //$('#'+valor).toggle();
+        $('#'+valor).fadeToggle(1000)
+    }
+
+	function detalleTarea(valores){
+        var idregistro = valores.view.params.idEmpd; 
+        //$("#detalle").text(registro); 
+		
+        var detalleOrden = new kendo.data.DataSource({
+            transport: {
+                    read: "http://54.207.102.124:8980/Customer/rest/Customer/Read",
+                	dataType: "jsonp"
+                    },
+                    schema: {
+                        data: function (data) {
+                            return data.response.dsCustomer.dsCustomer.ttCustomer;
+                    }
+                },
+            filter: { field: "CustNum", operator: "eq", value: parseInt(idregistro) }
+        });
+         
+
+        $("#detalle").kendoListView({
+            dataSource: detalleOrden,
+            template: kendo.template($("#tema002").html())
+        });
     }
 
 // END_CUSTOM_CODE_funcionalidad03
