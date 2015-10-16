4887f085346067b21072049ca831fd916268a54f
Erik Gadea <erik.gadea@valmar.com.pe>
2015-10-16 16:59:44 +0000

    Changes made by Erik Gadea (erik.gadea@valmar.com.pe).

--- App Movil/components/funcionalidad02/index.js
+++ App Movil/components/funcionalidad02/index.js
@@ -1,39 +1,55 @@
 document.addEventListener("deviceready", onDeviceReady, false);
  
-
 function onDeviceReady() {
-    navigator.notification.alert(navigator.device.capture);
+    //navigator.notification.alert(navigator.device.capture);
     navigator.splashscreen.hide();
     captureApp = new captureApp();
     captureApp.run();
-    //navigator.notification.alert("entro");
 }
 
 function captureApp() {
 }
 
-//captureApp.prototype = {
-  //  navigator.notification.alert("entro");
-//}
+window.audio = kendo.observable({
+ seleccionar: function() {
+     var that = this;
+     navigator.device.capture.captureAudio(function(capturedFiles) {
+         //navigator.notification.alert(capturedFiles);
+         //captureSuccess.apply(that, arguments);
+         
+        var apiKey = "Mosi1tOaeZ84K0L1";
+    	var el = new Everlive(apiKey);
+         
+          var options = {
+            fileName: 'audio.3gpp',
+            mimeType: 'audio/3gpp'
+         };
+         
+        el.files.upload(capturedFiles[0].localURL ,options)
+        .then(function() {
+            window.plugins.toast.showShortCenter("Upload");
+        }, function(err) {
+            window.plugins.toast.showShortCenter(err);
+        });
+     }, function() {
+         navigator.notification.alert("Problemas con el APP");
+     }, {limit:1});
+
+  }
+});
 
 
-   window.audio = kendo.observable({
-     seleccionar: function() {
-         //navigator.notification.alert("entro");
-         var that = this;
-         navigator.device.capture.captureAudio(function() {
-         //that._captureSuccess.apply(that, arguments);
-                var i,
-                media = document.getElementById("media");
-                media.innerHTML = "";
-                for (i=0;i < capturedFiles.length;i+=1) {
-                    media.innerHTML+='<p>Capture taken! Its path is: ' + capturedFiles[i].fullPath + '</p>'
-                }
-           
-         }, function() {
-             navigator.notification.alert("Problemas con el APP");
-         }, {limit:1});
-
-      }
-    });
-
+function otro(capturedFiles) {
+    navigator.notification.alert("entro 22222");
+        //var options = {
+            //fileName: 'audio.3gpp',
+            //mimeType: 'audio/3gpp'
+         //};
+         
+        //el.files.upload(capturedFiles[0].fullPath ,options)
+        //.then(function() {
+            //window.plugins.toast.showShortCenter("Upload");
+        //}, function(err) {
+            //window.plugins.toast.showShortCenter(err);
+        //});
+}
