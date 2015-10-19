6c046567afa5972fea125e463536f6bf8d0e387e
Josue Emanuel Mosquera <josue.mosquera@valmar.com.pe>
2015-10-19 15:47:27 +0000

    Changes made by Josue Emanuel Mosquera (josue.mosquera@valmar.com.pe).

--- App Movil/components/funcionalidad03/index.js
+++ App Movil/components/funcionalidad03/index.js
@@ -13,17 +13,12 @@ app.funcionalidad03 = kendo.observable({
     //Carga JavaScript 1st
 	function cargaEmpleados (){         
         var datosEmpleados = new kendo.data.DataSource({
+                type: "odata",
                 transport: {
-                    read: "http://54.207.102.124:8980/Customer/rest/Customer/Read",
-                    dataType: "jsonp"
-                    },
-            	schema: {
-                    data: function (data) {
-                    	return data.response.dsCustomer.dsCustomer.ttCustomer;
-                    }
+                    read: "http://demos.telerik.com/kendo-ui/service/Northwind.svc/Employees"
                 }
             });
-
+        
         $("#tareas").kendoListView({
             dataSource: datosEmpleados,
             template: kendo.template($("#tema001").html())
@@ -32,31 +27,34 @@ app.funcionalidad03 = kendo.observable({
 
 	function visibles(valor){
         //$('#'+valor).toggle();
-        $('#'+valor).fadeToggle(1000)
+        $('#'+valor).fadeToggle(600)
     }
 
 	function detalleTarea(valores){
-        var idregistro = valores.view.params.idEmpd; 
-        //$("#detalle").text(registro); 
+        var idregistro = parseInt(valores.view.params.idEmpd);
 		
         var detalleOrden = new kendo.data.DataSource({
+            type: "odata",
             transport: {
-                    read: "http://54.207.102.124:8980/Customer/rest/Customer/Read",
-                	dataType: "jsonp"
-                    },
-                    schema: {
-                        data: function (data) {
-                            return data.response.dsCustomer.dsCustomer.ttCustomer;
-                    }
-                },
-            filter: { field: "CustNum", operator: "eq", value: parseInt(idregistro) }
+                read: "http://demos.telerik.com/kendo-ui/service/Northwind.svc/Orders"
+            },
+            schema: {
+            	data: "d.results"
+            },
+            filter: { // filter is applied outside of the transport option
+                field: "EmployeeID", operator: "eq", value: idregistro
+            }
         });
-         
 
         $("#detalle").kendoListView({
             dataSource: detalleOrden,
             template: kendo.template($("#tema002").html())
         });
+    }
+
+	function regresarPantalla(btnregresar,divdatos){ 
+        $(divdatos).text("");
+        window.location.href = btnregresar;        
     }
 
 // END_CUSTOM_CODE_funcionalidad03
