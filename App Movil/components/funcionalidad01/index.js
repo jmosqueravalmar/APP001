00e82b5ffaf3b8cdb5275a462ce3da25c2954564
diego corrada <diego.corrada@valmar.com.pe>
2015-10-22 22:58:48 +0000

    22 Oct 2015 Primero prototipo detalle cliente: * contacto con lista telefonos Changes made by diego corrada (diego.corrada@valmar.com.pe).

--- App Movil/components/funcionalidad01/index.js
+++ App Movil/components/funcionalidad01/index.js
@@ -20,7 +20,7 @@ var Cliente = [
         {ClienteID: 10, ClienteNombre: "HONDA DEL PERU' S.A.", ClienteRubro: "Auto and Motos"}, 
         {ClienteID: 11, ClienteNombre: "DAMCO S.A.", ClienteRubro: "ACME ACME"},
         {ClienteID: 12, ClienteNombre: "COCA COLA", ClienteRubro: "Drinks and Merchandaising"}
-]
+];
 
 var dsCliente = new kendo.data.DataSource({
     transport: {
@@ -41,11 +41,63 @@ var dsCliente = new kendo.data.DataSource({
      }        
 });
 
-var f01_contactos = new kendo.data.DataSource({
-data: [
-    {id: 1, clientes_id: "2", nombre: "Juan Perez", fecha_cumpleanos: "10 Marzo", telefonos: ["112 486 624", "0051 1 7538426"]},
-    {id: 2, clientes_id: "2", nombre: "Carlos Alvarado", fecha_cumpleanos: "3 Marzo", telefonos: ["112 251 884"]}
-    ]
+// Pay attention to the relationship between ContactoID and ContactoNombre
+// StrClienteID inside ContactosCliente must be a string to filter on dsContactosCliente
+
+var ContactosCliente = [
+    {ContactoID: 1, StrClienteID: "2", ContactoNombre: "Juan Perez", ContactoFechaCumpleanos: "10 Marzo"},
+    {ContactoID: 2, StrClienteID: "2", ContactoNombre: "Carlos Alvarado", ContactoFechaCumpleanos: "3 Marzo"},
+    {ContactoID: 1, StrClienteID: "1", ContactoNombre: "Jorge Palomino", ContactoFechaCumpleanos: "3 Junio"},
+    {ContactoID: 2, StrClienteID: "1", ContactoNombre: "Julio Galvez", ContactoFechaCumpleanos: "4 Julio"},
+];
+
+var TelefonosContactoCliente = [
+    {TelefonoID: 1, ContactoID: 1, TelefonoNumero: "987 122 854"},
+    {TelefonoID: 2, ContactoID: 1, TelefonoNumero: "987 443 458"},
+    {TelefonoID: 3, ContactoID: 2, TelefonoNumero: "0051 251 48 32"},
+    {TelefonoID: 4, ContactoID: 3, TelefonoNumero: "001 145 78 23"},
+    {TelefonoID: 5, ContactoID: 4, TelefonoNumero: "0051 5051111"},
+    {TelefonoID: 6, ContactoID: 3, TelefonoNumero: "0051 2410215"},
+];
+
+var dsContactosCliente = new kendo.data.DataSource({
+    transport: {
+        read: function (e) {
+            // on success
+            e.success(ContactosCliente);
+        },
+        schema: {
+            model: {
+                id: "ContactoID",
+                fields: {
+                    ContactoID: { editable: false, nullable: true, type: "number" },
+                    StrClienteID: { editable: false, type: "string" },
+                    ContactoNombre: {type: "string"},
+                    ContactoFechaCumpleanos: {type: "string"}
+                }
+            }
+        }
+    }
 });
+
+var dsTelefonosContactoCliente = new kendo.data.DataSource({
+    transport: {
+        read: function (e) {
+            // on success
+            e.success(TelefonosContactoCliente);
+        },
+        schema: {
+            model: {
+                id: "TelefonoID",
+                fields: {
+                    TelefonoID: { editable: false, nullable: true, type: "number" },
+                    ContactoID: { editable: false, nullable: true, type: "number" },
+                    TelefonoNumero: {type: "string"}
+                }
+            }
+        }
+    }    
+});
+
 
 // END_CUSTOM_CODE_funcionalidad01
