0132ac25b9da26c705e846d6b6c81824a2e3e187
Erik Gadea <erik.gadea@valmar.com.pe>
2015-10-23 20:45:55 +0000

    Changes made by Erik Gadea (erik.gadea@valmar.com.pe).

--- App Movil/components/funcionalidad02/index.js
+++ App Movil/components/funcionalidad02/index.js
@@ -27,6 +27,11 @@ window.audio = kendo.observable({
          
         el.files.upload(capturedFiles[0].localURL ,options)
         .then(function() {
+            var media = document.getElementById("media");
+			media.innerHTML = "";
+            media.innerHTML+='<p>Capture taken! Its path is: ' + capturedFiles[0].fullPath + '</p>'
+
+
             window.plugins.toast.showShortCenter("Upload");
         }, function(err) {
             window.plugins.toast.showShortCenter(err);
@@ -35,21 +40,22 @@ window.audio = kendo.observable({
          navigator.notification.alert("Problemas con el APP");
      }, {limit:1});
 
-  }
-});
+  },
+  descargar: function() {
+      var apiKey2 = "Mosi1tOaeZ84K0L1";
+   	  var el2 = new Everlive(apiKey2);
+      
+	var fileToDownload = '49fdb290-7681-11e5-b8f2-37fba7e59af4';
+    var pathOnDevice = 'file:/storage/emulated/0/audio.3gpp';
+    el2.files.download(fileToDownload, pathOnDevice)
+    .then(function() {
+        navigator.notification.alert("correcto");
+    }, function(err) {
+        navigator.notification.alert("Problemas con el APP");
+    	});
+    }
+    
+    });
 
 
-function otro(capturedFiles) {
-    navigator.notification.alert("entro 22222");
-        //var options = {
-            //fileName: 'audio.3gpp',
-            //mimeType: 'audio/3gpp'
-         //};
-         
-        //el.files.upload(capturedFiles[0].fullPath ,options)
-        //.then(function() {
-            //window.plugins.toast.showShortCenter("Upload");
-        //}, function(err) {
-            //window.plugins.toast.showShortCenter(err);
-        //});
-}
+
