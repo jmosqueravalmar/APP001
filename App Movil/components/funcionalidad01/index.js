24acf8bd3dad1206d09d431a24491c5b2c7cce4f
diego corrada <diego.corrada@valmar.com.pe>
2015-11-02 23:11:54 +0000

    Error con primera carga de datos Changes made by diego corrada (diego.corrada@valmar.com.pe).

--- App Movil/components/funcionalidad01/index.js
+++ App Movil/components/funcionalidad01/index.js
@@ -9,10 +9,8 @@ app.funcionalidad01 = kendo.observable({
 
 var Cliente = [
         {ClienteID: 199, ClienteRazonSocial: "EXSA S.A."},
-        {ClienteID: 1605, ClienteRazonSocial: "EXTINTORES PALVA S.C.R.L."}, 
-        {ClienteID: 1609, ClienteRazonSocial: "EXTRACTOS Y OLEORRESINAS NATURALES SOCIEDAD ANÓNIMA CERRADA - EXNAT S.A.C."},
-        {ClienteID: 761, ClienteRazonSocial: "EYR-UNION-AYB-ASOCIADOS"}, 
-        {ClienteID: 1658, ClienteRazonSocial: "FABIOLA CATHERINE CHUECAS BECERRA"}, 
+        {ClienteID: 459, ClienteRazonSocial: "SODIMAC PERU S.A."}, 
+        {ClienteID: 483, ClienteRazonSocial: "TIENDAS PERUANAS S.A."},
 ];
 
 var dsCliente = new kendo.data.DataSource({
@@ -23,7 +21,7 @@ var dsCliente = new kendo.data.DataSource({
         },
         schema: {
             model: {
-                id: "ClienteID",
+                id: "rsID",
                 fields: {
                     ClienteID: { editable: false, nullable: true, type: "number" },
                     ClienteNombre: {type: "string"},
@@ -35,22 +33,15 @@ var dsCliente = new kendo.data.DataSource({
 });
 
 // Pay attention to the relationship between ContactoID and ContactoNombre
-// StrClienteID inside ContactosCliente must be a string to filter on dsContactosCliente
 
 var ContactosCliente = [
-    {ContactoID: 1, StrClienteID: "199", ContactoNombre: "Juan Perez", ContactoFechaCumpleanos: "10 Marzo"},
-    {ContactoID: 2, StrClienteID: "199", ContactoNombre: "Carlos Alvarado", ContactoFechaCumpleanos: "3 Marzo"},
-    {ContactoID: 1, StrClienteID: "1605", ContactoNombre: "Jorge Palomino", ContactoFechaCumpleanos: "3 Junio"},
-    {ContactoID: 2, StrClienteID: "1605", ContactoNombre: "Julio Galvez", ContactoFechaCumpleanos: "4 Julio"},
-];
-
-var TelefonosContactoCliente = [
-    {TelefonoID: 1, ContactoID: 1, TelefonoNumero: "987 122 854"},
-    {TelefonoID: 2, ContactoID: 1, TelefonoNumero: "987 443 458"},
-    {TelefonoID: 3, ContactoID: 2, TelefonoNumero: "0051 251 48 32"},
-    {TelefonoID: 4, ContactoID: 3, TelefonoNumero: "001 145 78 23"},
-    {TelefonoID: 5, ContactoID: 4, TelefonoNumero: "0051 5051111"},
-    {TelefonoID: 6, ContactoID: 3, TelefonoNumero: "0051 2410215"},
+    {rsID: 1, ClienteID: 199, ContactoID: 1, ContactoNombre: "Francisco Jose Vasquez", ContactoFechaCumpleanos: "4/11"},
+    {rsID: 2, ClienteID: 199, ContactoID: 2, ContactoNombre: "Martin Hugo Barrientos", ContactoFechaCumpleanos: "29/07"},
+    {rsID: 3, ClienteID: 199, ContactoID: 3, ContactoNombre: "Javier Becerra", ContactoFechaCumpleanos: "N/A"},
+    {rsID: 4, ClienteID: 459, ContactoID: 1,  ContactoNombre: "Ingrid Helga Valz", ContactoFechaCumpleanos: "N/A"},
+    {rsID: 5, ClienteID: 459, ContactoID: 2, ContactoNombre: "Ivan Chavez", ContactoFechaCumpleanos: "25/2"},
+    {rsID: 6, ClienteID: 459, ContactoID: 3, ContactoNombre: "Katy Sandy Prado", ContactoFechaCumpleanos: "4/5"},
+    {rsID: 7, ClienteID: 459, ContactoID: 4, ContactoNombre: "Monica Cabrera", ContactoFechaCumpleanos: "26/10"},
 ];
 
 var dsContactosCliente = new kendo.data.DataSource({
@@ -63,8 +54,8 @@ var dsContactosCliente = new kendo.data.DataSource({
             model: {
                 id: "ContactoID",
                 fields: {
+                    ClienteID: { editable: false, type: "number" },
                     ContactoID: { editable: false, nullable: true, type: "number" },
-                    StrClienteID: { editable: false, type: "string" },
                     ContactoNombre: {type: "string"},
                     ContactoFechaCumpleanos: {type: "string"}
                 }
@@ -72,6 +63,21 @@ var dsContactosCliente = new kendo.data.DataSource({
         }
     }
 });
+
+var TelefonosContactoCliente = [
+    {TelefonoID: 1, ClienteID: 199, ContactoID: 1, TelefonoNumero: "3157000"},
+    {TelefonoID: 2, ClienteID: 199, ContactoID: 2, TelefonoNumero: "3157000 Ext.2330"},
+    {TelefonoID: 3, ClienteID: 199, ContactoID: 3, TelefonoNumero: "3157000 Ext.2390"},
+    {TelefonoID: 4, ClienteID: 199, ContactoID: 3, TelefonoNumero: "989208014"},
+    /* {TelefonoID: 5, ClienteID: 459, ContactoID: 1, TelefonoNumero: "2119500 Ext.1405"},
+    {TelefonoID: 6, ClienteID: 459, ContactoID: 1, TelefonoNumero: "97753267"},
+    {TelefonoID: 7, ClienteID: 459, ContactoID: 1, TelefonoNumero: "109*2599"},
+    {TelefonoID: 8, ClienteID: 459, ContactoID: 2, TelefonoNumero: "N/A"},
+    {TelefonoID: 9, ClienteID: 459, ContactoID: 3, TelefonoNumero: "2119500"},
+    {TelefonoID: 10, ClienteID: 459, ContactoID: 3, TelefonoNumero: "986828093"},
+    {TelefonoID: 11, ClienteID: 459, ContactoID: 4, TelefonoNumero: "6110400 Ext.430"}, */
+
+];
 
 var dsTelefonosContactoCliente = new kendo.data.DataSource({
     transport: {
@@ -84,6 +90,7 @@ var dsTelefonosContactoCliente = new kendo.data.DataSource({
                 id: "TelefonoID",
                 fields: {
                     TelefonoID: { editable: false, nullable: true, type: "number" },
+                    ClienteID: { editable: false, type: "number" },
                     ContactoID: { editable: false, nullable: true, type: "number" },
                     TelefonoNumero: {type: "string"}
                 }
