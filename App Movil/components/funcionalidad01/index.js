d85c93635817d7efa2bfa0391d34ee337b8983d8
diego corrada <diego.corrada@valmar.com.pe>
2015-11-03 17:01:19 +0000

    Problema de la 1era carga de contactos y telefonos....
    Changes made by diego corrada (diego.corrada@valmar.com.pe).

--- App Movil/components/funcionalidad01/index.js
+++ App Movil/components/funcionalidad01/index.js
@@ -21,7 +21,7 @@ var dsCliente = new kendo.data.DataSource({
         },
         schema: {
             model: {
-                id: "rsID",
+                id: "ClienteID",
                 fields: {
                     ClienteID: { editable: false, nullable: true, type: "number" },
                     ClienteNombre: {type: "string"},
@@ -33,15 +33,16 @@ var dsCliente = new kendo.data.DataSource({
 });
 
 // Pay attention to the relationship between ContactoID and ContactoNombre
+// rsID non serve tanto
 
 var ContactosCliente = [
-    {rsID: 1, ClienteID: 199, ContactoID: 1, ContactoNombre: "Francisco Jose Vasquez", ContactoFechaCumpleanos: "4/11"},
-    {rsID: 2, ClienteID: 199, ContactoID: 2, ContactoNombre: "Martin Hugo Barrientos", ContactoFechaCumpleanos: "29/07"},
-    {rsID: 3, ClienteID: 199, ContactoID: 3, ContactoNombre: "Javier Becerra", ContactoFechaCumpleanos: "N/A"},
-    {rsID: 4, ClienteID: 459, ContactoID: 1,  ContactoNombre: "Ingrid Helga Valz", ContactoFechaCumpleanos: "N/A"},
-    {rsID: 5, ClienteID: 459, ContactoID: 2, ContactoNombre: "Ivan Chavez", ContactoFechaCumpleanos: "25/2"},
-    {rsID: 6, ClienteID: 459, ContactoID: 3, ContactoNombre: "Katy Sandy Prado", ContactoFechaCumpleanos: "4/5"},
-    {rsID: 7, ClienteID: 459, ContactoID: 4, ContactoNombre: "Monica Cabrera", ContactoFechaCumpleanos: "26/10"},
+    {ClienteID: 199, ContactoID: 1, ContactoNombre: "Francisco Jose Vasquez", ContactoFechaCumpleanos: "4/11"},
+    {ClienteID: 199, ContactoID: 2, ContactoNombre: "Martin Hugo Barrientos", ContactoFechaCumpleanos: "29/07"},
+    {ClienteID: 199, ContactoID: 3, ContactoNombre: "Javier Becerra", ContactoFechaCumpleanos: "N/A"},
+    {ClienteID: 459, ContactoID: 1,  ContactoNombre: "Ingrid Helga Valz", ContactoFechaCumpleanos: "N/A"},
+    {ClienteID: 459, ContactoID: 2, ContactoNombre: "Ivan Chavez", ContactoFechaCumpleanos: "25/2"},
+    {ClienteID: 459, ContactoID: 3, ContactoNombre: "Katy Sandy Prado", ContactoFechaCumpleanos: "4/5"},
+    {ClienteID: 459, ContactoID: 4, ContactoNombre: "Monica Cabrera", ContactoFechaCumpleanos: "26/10"},
 ];
 
 var dsContactosCliente = new kendo.data.DataSource({
@@ -54,8 +55,8 @@ var dsContactosCliente = new kendo.data.DataSource({
             model: {
                 id: "ContactoID",
                 fields: {
-                    ClienteID: { editable: false, type: "number" },
                     ContactoID: { editable: false, nullable: true, type: "number" },
+                    ClienteID: { editable: false, type: "number" },
                     ContactoNombre: {type: "string"},
                     ContactoFechaCumpleanos: {type: "string"}
                 }
@@ -69,10 +70,10 @@ var TelefonosContactoCliente = [
     {TelefonoID: 2, ClienteID: 199, ContactoID: 2, TelefonoNumero: "3157000 Ext.2330"},
     {TelefonoID: 3, ClienteID: 199, ContactoID: 3, TelefonoNumero: "3157000 Ext.2390"},
     {TelefonoID: 4, ClienteID: 199, ContactoID: 3, TelefonoNumero: "989208014"},
-    /* {TelefonoID: 5, ClienteID: 459, ContactoID: 1, TelefonoNumero: "2119500 Ext.1405"},
+    {TelefonoID: 5, ClienteID: 459, ContactoID: 1, TelefonoNumero: "2119500 Ext.1405"},
     {TelefonoID: 6, ClienteID: 459, ContactoID: 1, TelefonoNumero: "97753267"},
     {TelefonoID: 7, ClienteID: 459, ContactoID: 1, TelefonoNumero: "109*2599"},
-    {TelefonoID: 8, ClienteID: 459, ContactoID: 2, TelefonoNumero: "N/A"},
+    /* {TelefonoID: 8, ClienteID: 459, ContactoID: 2, TelefonoNumero: "N/A"},
     {TelefonoID: 9, ClienteID: 459, ContactoID: 3, TelefonoNumero: "2119500"},
     {TelefonoID: 10, ClienteID: 459, ContactoID: 3, TelefonoNumero: "986828093"},
     {TelefonoID: 11, ClienteID: 459, ContactoID: 4, TelefonoNumero: "6110400 Ext.430"}, */
