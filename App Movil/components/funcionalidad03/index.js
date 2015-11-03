437410614264e1995aab334df24cd0c62efc114a
Josue Emanuel Mosquera <josue.mosquera@valmar.com.pe>
2015-11-03 20:53:27 +0000

    Changes made by Josue Emanuel Mosquera (josue.mosquera@valmar.com.pe).

--- App Movil/components/funcionalidad03/index.js
+++ App Movil/components/funcionalidad03/index.js
@@ -12,19 +12,19 @@ app.funcionalidad03 = kendo.observable({
     
 	var ListTareas = new kendo.data.DataSource({ 
                         data: [
-                            {id: 1,  idTTarea: 10,nombre: "Tarea Nro 1", cliente: "Cliente 1", fcreacion: "01/10/2015", flimite: "01/10/2015", estado: "1", prioridad: "1"},
-                            {id: 2,  idTTarea: 10,nombre: "Tarea Nro 2", cliente: "Cliente 2",fcreacion: "01/10/2015", flimite: "01/10/2015", estado: "2", prioridad: "1"},
-                            {id: 3,  idTTarea: 3,nombre: "Tarea Nro 3", cliente: "Cliente 3",fcreacion: "01/10/2015", flimite: "01/10/2015", estado: "3", prioridad: "2"},
-                            {id: 4,  idTTarea: 4,nombre: "Tarea Nro 4", cliente: "Cliente 4",fcreacion: "01/12/2015", flimite: "01/10/2015", estado: "1", prioridad: "1"},
-                            {id: 5,  idTTarea: 5,nombre: "Tarea Nro 5", cliente: "Cliente 5",fcreacion: "01/10/2015", flimite: "01/10/2015", estado: "2", prioridad: "2"},
-                            {id: 6,  idTTarea: 6,nombre: "Tarea Nro 6", cliente: "Cliente 6",fcreacion: "01/10/2015", flimite: "01/10/2015", estado: "3", prioridad: "2"},
-                            {id: 7,  idTTarea: 7,nombre: "Tarea Nro 7", cliente: "Cliente 7",fcreacion: "01/12/2015", flimite: "01/10/2015", estado: "1", prioridad: "1"},
-                            {id: 8,  idTTarea: 8,nombre: "Tarea Nro 8", cliente: "Cliente 8",fcreacion: "01/10/2015", flimite: "01/10/2015", estado: "2", prioridad: "2"},
-                            {id: 9,  idTTarea: 9,nombre: "Tarea Nro 9", cliente: "Cliente 9",fcreacion: "01/10/2015", flimite: "01/10/2015", estado: "3", prioridad: "1"},
-                            {id: 10, idTTarea: 10,nombre: "Tarea Nro 10", cliente: "Cliente 10",fcreacion: "01/10/2015", flimite: "01/10/2015", estado: "1", prioridad: "3"},
-                            {id: 11, idTTarea: 11,nombre: "Tarea Nro 11", cliente: "Cliente 11",fcreacion: "01/10/2015", flimite: "01/10/2015", estado: "2", prioridad: "1"},
-                            {id: 12, idTTarea: 12,nombre: "Tarea Nro 12", cliente: "Cliente 12",fcreacion: "01/10/2015", flimite: "01/10/2015", estado: "3", prioridad: "3"},
-                            {id: 13, idTTarea: 13,nombre: "Tarea Nro 13", cliente: "Cliente 13",fcreacion: "01/10/2015", flimite: "01/10/2015", estado: "1", prioridad: "3"}
+                           		{id: 1,  idTTarea: 10,nombre: "Tarea Nro 1", cliente: "Cliente 1", fcreacion: "01/10/2015", flimite: "01/10/2015", estado: "pendiente", prioridad: "alto"},
+                                {id: 2,  idTTarea: 10,nombre: "Tarea Nro 2", cliente: "Cliente 2",fcreacion: "01/10/2015", flimite: "01/10/2015",  estado: "pendiente", prioridad: "medio"},
+                                {id: 3,  idTTarea: 3,nombre: "Tarea Nro 3", cliente: "Cliente 3",fcreacion: "01/10/2015", flimite: "01/10/2015",   estado: "pendiente", prioridad: "alto"},
+                                {id: 4,  idTTarea: 4,nombre: "Tarea Nro 4", cliente: "Cliente 4",fcreacion: "01/12/2015", flimite: "01/10/2015",   estado: "pendiente", prioridad: "alto"},
+                                {id: 5,  idTTarea: 5,nombre: "Tarea Nro 5", cliente: "Cliente 5",fcreacion: "01/10/2015", flimite: "01/10/2015",   estado: "pendiente", prioridad: "alto"},
+                                {id: 6,  idTTarea: 6,nombre: "Tarea Nro 6", cliente: "Cliente 6",fcreacion: "01/10/2015", flimite: "01/10/2015",   estado: "pendiente", prioridad: "bajo"},
+                                {id: 7,  idTTarea: 7,nombre: "Tarea Nro 7", cliente: "Cliente 7",fcreacion: "01/12/2015", flimite: "01/10/2015",   estado: "pendiente", prioridad: "alto"},
+                                {id: 8,  idTTarea: 8,nombre: "Tarea Nro 8", cliente: "Cliente 8",fcreacion: "01/10/2015", flimite: "01/10/2015",   estado: "pendiente", prioridad: "alto"},
+                                {id: 9,  idTTarea: 9,nombre: "Tarea Nro 9", cliente: "Cliente 9",fcreacion: "01/10/2015", flimite: "01/10/2015",   estado: "pendiente", prioridad: "medio"},
+                                {id: 10, idTTarea: 10,nombre: "Tarea Nro 10", cliente: "Cliente 10",fcreacion: "01/10/2015", flimite: "01/10/2015",estado: "pendiente", prioridad: "medio"},
+                                {id: 11, idTTarea: 11,nombre: "Tarea Nro 11", cliente: "Cliente 11",fcreacion: "01/10/2015", flimite: "01/10/2015",estado: "pendiente", prioridad: "bajo"},
+                                {id: 12, idTTarea: 12,nombre: "Tarea Nro 12", cliente: "Cliente 12",fcreacion: "01/10/2015", flimite: "01/10/2015",estado: "pendiente", prioridad: "alto"},
+                                {id: 13, idTTarea: 13,nombre: "Tarea Nro 13", cliente: "Cliente 13",fcreacion: "01/10/2015", flimite: "01/10/2015",estado: "pendiente", prioridad: "alto"}
                         ],
         				pageSize: 8
 					 });
