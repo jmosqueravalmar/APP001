d34d3e8e13ed9330bd40ecd040e8ae7e068a4ec2
enrique malaver <victor.malaver@valmar.com.pe>
2015-11-06 22:01:00 +0000

    Changes made by enrique malaver (victor.malaver@valmar.com.pe).

--- App Movil/components/funcionalidad02/index.js
+++ App Movil/components/funcionalidad02/index.js
@@ -1,61 +1,140 @@
-document.addEventListener("deviceready", onDeviceReady, false);
- 
-function onDeviceReady() {
-    //navigator.notification.alert(navigator.device.capture);
-    navigator.splashscreen.hide();
-    captureApp = new captureApp();
-    captureApp.run();
-}
+'use strict';
 
-function captureApp() {
-}
+app.funcionalidad06 = kendo.observable({
+    onShow: function () {},
+    afterShow: function () {},
+});
 
-window.audio = kendo.observable({
- seleccionar: function() {
-     var that = this;
-     navigator.device.capture.captureAudio(function(capturedFiles) {
-         //navigator.notification.alert(capturedFiles);
-         //captureSuccess.apply(that, arguments);
-         
-        var apiKey = "Mosi1tOaeZ84K0L1";
-    	var el = new Everlive(apiKey);
-         
-          var options = {
-            fileName: 'audio.3gpp',
-            mimeType: 'audio/3gpp'
-         };
-         
-        el.files.upload(capturedFiles[0].localURL ,options)
-        .then(function() {
-            var media = document.getElementById("media");
-			media.innerHTML = "";
-            media.innerHTML+='<p>Capture taken! Its path is: ' + capturedFiles[0].fullPath + '</p>'
-
-
-            window.plugins.toast.showShortCenter("Upload");
-        }, function(err) {
-            window.plugins.toast.showShortCenter(err);
-        });
-     }, function() {
-         navigator.notification.alert("Problemas con el APP");
-     }, {limit:1});
-
-  },
-  descargar: function() {
-      var apiKey2 = "Mosi1tOaeZ84K0L1";
-   	  var el2 = new Everlive(apiKey2);
-      
-	var fileToDownload = '49fdb290-7681-11e5-b8f2-37fba7e59af4';
-    var pathOnDevice = 'file:/storage/emulated/0/audio.3gpp';
-    el2.files.download(fileToDownload, pathOnDevice)
-    .then(function() {
-        navigator.notification.alert("correcto");
-    }, function(err) {
-        navigator.notification.alert("Problemas con el APP");
-    	});
-    }
-    
+function getOrden(year, order) {
+    console.log(order);
+    var dsOrden = new kendo.data.DataSource({
+        transport: {
+            read: {
+                url: "http://www.ausa.com.pe/appmovil_test01/Ordenes/valor?fecha=" + year + "&id=" + order,
+                dataType: "json"
+            }
+        },
+        schema: {
+            data: function (data) {
+                return data;
+            }
+        }
     });
 
+    dsOrden.fetch(function () {
+        if (dsOrden.total() > 0) { //Si existe la órden
+            var data = dsOrden.data();
+            var orden = data[0];
+            var dsDetOrden = new kendo.data.DataSource({
+                transport: {
+                    read: {
+                        url: "http://www.ausa.com.pe/appmovil_test01/Ordenes/detalle/" + orden.ord_int_id,
+                        dataType: "json"
+                    }
+                },
+                schema: {
+                    data: function (data) {
+                        return data;
+                    },
+                    model: {
+                        fields: {
+                            Apertura: { //Inicio Importación
+                                type: "date"
+                            },
+                            ETAEstimado: {
+                                type: "date"
+                            },
+                            LlegadaNave: {
+                                type: "date"
+                            },
+                            TerminoDescarga: {
+                                type: "date"
+                            },
+                            DocumentosCompletos: {
+                                type: "date"
+                            },
+                            FechaNumeracion: {
+                                type: "date"
+                            },
+                            FechaCancelacion: {
+                                type: "date"
+                            },
+                            AforoCulminado: {
+                                type: "date"
+                            },
+                            LevanteAutorizado: {
+                                type: "date"
+                            },
+                            RetiroCulminado: {
+                                type: "date"
+                            },
+                            IniciarTramite: { //Inicio Exportación
+                                type: "date"
+                            },
+                            ESC: {
+                                type: "date"
+                            },
+                            DAM: {
+                                type: "date"
+                            },
+                            DAMFisico: {
+                                type: "date"
+                            },
+                            MercanciaPuerto: {
+                                type: "date"
+                            },
+                            EmbarqueExito: {
+                                type: "date"
+                            },
+                            TramiteVisto: {
+                                type: "date"
+                            },
+                            RecepDocs: {
+                                type: "date"
+                            },
+                            DAMDefinitiva: {
+                                type: "date"
+                            },
+                            Regularizacion: {
+                                type: "date"
+                            },
+                            DAMcliente: {
+                                type: "date"
+                            }
+                        }
+                    }
+                }
+            });
+            dsDetOrden.fetch(function () {
+                var data = dsDetOrden.data();
+                var detOrden = data[0];
+                if (detOrden.Regimen == "IMPO") {
+                    $("#det-orden").kendoListView({
+                        dataSource: dsDetOrden,
+                        template: kendo.template($("#temp01").html())
+                    });
+                } else {
+                    $("#det-orden").kendoListView({
+                        dataSource: dsDetOrden,
+                        template: kendo.template($("#temp02").html())
+                    });
+                }
+            });
+            window.location.href = "#det-orden1";
+        } else {
+            var notificationElement = $("#notification");
+            notificationElement.kendoNotification();
+            var notificationWidget = notificationElement.data("kendoNotification");
+            notificationWidget.show("La órden no existe", "error");
+        }
+    });
+    /*dsOrden.fetch(function () {
+        var data = dsOrden.data();
+        var orden = data[0];
 
+        var date = new Date(parseInt(orden.fecha, 10));
 
+        //alert(date);
+        //alert(date.toLocaleString());
+    });*/
+}
