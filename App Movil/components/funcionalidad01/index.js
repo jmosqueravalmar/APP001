70f645e140879935cf99ebb4344a9af3f0ed9c4b
diego corrada <diego.corrada@valmar.com.pe>
2015-11-09 23:36:37 +0000

    Continuare implementado il Data Source dati Situaccion de Pago Changes made by diego corrada (diego.corrada@valmar.com.pe).

--- App Movil/components/funcionalidad01/index.js
+++ App Movil/components/funcionalidad01/index.js
@@ -1,98 +1,134 @@
 'use strict';
 
 app.funcionalidad01 = kendo.observable({
-    onShow: function() {},
+    beforeShow: function() {
+        console.log("DFC > beforeShow ");        
+    },
+    onShow: function() {
+
+    },
     afterShow: function() {},    
+    MostraDetalleCliente: function() {
+        console.log("DFC > MostraDetalleCliente ");
+        console.log("Detalle Cliente ClienteID > " + ClienteID);
+        
+        dsContactosCliente = new kendo.data.DataSource({
+              transport: {
+                //Parametrizzare con ContactoID
+                read: {
+                    url: "http://www.ausa.com.pe/appmovil_test01/Clientes/contacto/"+ClienteID,
+                    dataType: "json"
+                 },
+              },
+              schema: {
+                  model: {
+                       id: "ContactoID",
+                       fields: {
+                           ContactoID: { editable: false, nullable: true, type: "number" },
+                           ContactoNombre: {type: "string"},
+                           ContactoFechaCumpleanos: {type: "string"}
+                       }
+                   }
+              },
+             requestEnd: function(e) {
+                console.log("dsContactosCliente >> requestEnd");
+             },
+         });
+
+        dsTelefonosContactoCliente = new kendo.data.DataSource({
+            transport: {
+                //Esempio
+                // http://www.ausa.com.pe/appmovil_test01/Clientes/contactoT?id=199&contacto=0
+                //Parametrizzare con ContactoID
+                read: {
+                    url: "http://www.ausa.com.pe/appmovil_test01/Clientes/contactoT?id="+ClienteID+"&contacto=0",
+                    dataType: "json"
+                 },
+            },
+            schema: {
+                model: {
+                    id: "TelefonoID",
+                    fields: {
+                        TelefonoID: { editable: false, nullable: true, type: "number" },
+                        ContactoID: { editable: false, nullable: true, type: "number" },
+                        ContactoNombre: {editable: false, type: "string"},
+                        Tipo: {editable: false, type: "string"},
+                        Numero: {type: "string"}
+                    }
+                }
+            },
+            requestEnd: function(e) {
+                console.log("dsTelefonosContactoCliente >> requestEnd");
+            },
+        });        
+        
+         $("#ContactosCliente").kendoDropDownList({
+            dataTextField: "ContactoNombre",
+            dataValueField: "ContactoID",
+            dataSource: dsContactosCliente,
+            change: function(e) {
+                console.log("ContactosCliente >> change");
+                // ContactoFechaCumpleanos
+                // get the dataItem corresponding to the selectedIndex.
+                $("#FechaCumpleanos").html($.trim(this.dataItem().ContactoFechaCumpleanos)); 
+            },
+            dataBound: function(e) {
+                console.log("ContactosCliente >> dataBound");
+                $("#FechaCumpleanos").html($.trim("1 May.")); 
+                // dataItem from dsContactosCliente DataSource
+                // console.log("ContactosCliente >> dataBound >> dataItem(0): " + this.dataItem(0).ContactoID + " -- " + this.dataItem(0).ContactoNombre + " -- " + this.dataItem(0).ContactoFechaCumpleanos);
+                // ContactoFechaCumpleanos
+                $("#FechaCumpleanos").html($.trim(this.dataItem(0).ContactoFechaCumpleanos));
+            }
+         });
+        
+         $("#TelefonosContactoCliente").kendoDropDownList({
+            cascadeFrom: "ContactosCliente",
+            dataTextField: "Numero",
+            dataValueField: "TelefonoID",
+            dataSource: dsTelefonosContactoCliente
+         });
+        
+         $("#btnLlamar").kendoButton({
+                 click: function(e) {
+                     console.log(e.event.target.tagName);                     
+                     // +++ ERROR +++ Simple reference to a JQuery object
+                     //console.log("Llamar numero >> " + $("#TelefonosContactoCliente").dataItem().TelefonoNumero);                     
+                     console.log("Llamar numero >> " + $("#TelefonosContactoCliente").data("kendoDropDownList").dataItem().Numero);
+                     window.open('tel:' + $("#TelefonosContactoCliente").data("kendoDropDownList").dataItem().Numero, '_system')
+                 }
+        });
+    
+    },
 });
 
 // START_CUSTOM_CODE_funcionalidad01
 
-var Cliente = [
-        {ClienteID: 199, ClienteRazonSocial: "EXSA S.A."},
-        {ClienteID: 459, ClienteRazonSocial: "SODIMAC PERU S.A."}, 
-        {ClienteID: 483, ClienteRazonSocial: "TIENDAS PERUANAS S.A."},
-];
-
 var dsCliente = new kendo.data.DataSource({
     transport: {
-        /*** */
         // OK funziona, ottimizzare per grandi vol. di dati || paginazione
+        // Parametrizzare la variabile idCliente
         read: {
             url: "http://www.ausa.com.pe/appmovil_test01/Clientes/cartera/305",
             dataType: "json"
-        },
-        
-        /*read: function (e) {
-            // on success
-            e.success(Cliente);
-        },*/
-        
-        schema: {
-            model: {
-                id: "ClienteID",
-                fields: {
-                    ClienteID: { editable: false, nullable: true, type: "number" },
-                    ClienteRazonSocial: {type: "string"},
-                }
+        },        
+     },
+    schema: {
+        model: {
+            id: "ClienteID",
+            fields: {
+                ClienteID: { editable: false, nullable: true, type: "number" },
+                ClienteRazonSocial: {type: "string"},
             }
         }
-     }        
+    },
+    // Filtro de prueba para desarrollo --- Eliminar en produccion!!!
+     filter: { field: "ClienteRazonSocial", operator: "startswith", value: "EX" }
 });
 
-// Pay attention to the relationship between ContactoID and ContactoNombre
-// Holds the contact list by 1 Client e.g.  ClienteID=199 EXSA S.A.
-
-var ContactosCliente = [
-    {ContactoID: 1, ContactoNombre: "Francisco Jose Vasquez", ContactoFechaCumpleanos: "4/11"},
-    {ContactoID: 2, ContactoNombre: "Martin Hugo Barrientos", ContactoFechaCumpleanos: "29/07"},
-    {ContactoID: 3, ContactoNombre: "Javier Becerra", ContactoFechaCumpleanos: "N/A"},
-];
-
-var dsContactosCliente = new kendo.data.DataSource({
-    transport: {
-        read: function (e) {
-            // on success
-            e.success(ContactosCliente);
-        },
-        schema: {
-            model: {
-                id: "ContactoID",
-                fields: {
-                    ContactoID: { editable: false, nullable: true, type: "number" },
-                    ContactoNombre: {type: "string"},
-                    ContactoFechaCumpleanos: {type: "string"}
-                }
-            }
-        }
-    }
-});
-
-// Holds the tel. nums. list of all contacts by 1 Client e.g.  ClienteID=199 EXSA S.A.
-
-var TelefonosContactoCliente = [
-    {TelefonoID: 1, ContactoID: 1, TelefonoNumero: "3157000"},
-    {TelefonoID: 2, ContactoID: 2, TelefonoNumero: "3157000 Ext.2330"},
-    {TelefonoID: 3, ContactoID: 3, TelefonoNumero: "3157000 Ext.2390"},
-    {TelefonoID: 4, ContactoID: 3, TelefonoNumero: "989208014"},
-];
-
-var dsTelefonosContactoCliente = new kendo.data.DataSource({
-    transport: {
-        read: function (e) {
-            // on success
-            e.success(TelefonosContactoCliente);
-        },
-        schema: {
-            model: {
-                id: "TelefonoID",
-                fields: {
-                    TelefonoID: { editable: false, nullable: true, type: "number" },
-                    ContactoID: { editable: false, nullable: true, type: "number" },
-                    TelefonoNumero: {type: "string"}
-                }
-            }
-        }
-    }    
-});
+var ClienteID = "";
+var dsContactosCliente = null;
+var dsTelefonosContactoCliente = null;
+var dsSituaccionPago = null;
 
 // END_CUSTOM_CODE_funcionalidad01
